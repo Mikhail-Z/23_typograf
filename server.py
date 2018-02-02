03a7994e13fc32ac0d476bc0f4c4f35e82e87293
mikhail-z <09041996a>
2018-02-02 09:53:51 +0300

    added csrf protection

--- server.py
+++ server.py
@@ -1,16 +1,29 @@
 from flask import Flask, render_template, request
 import typograph as tp
+from flask_wtf import FlaskForm
+from flask_wtf.csrf import CSRFProtect
+from wtforms.fields import TextAreaField
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'thisissecretkey'
+csrf = CSRFProtect()
+csrf.init_app(app)
+
+
+class Text4ChangeForm(FlaskForm):
+    input_text = TextAreaField()
 
 
 @app.route('/', methods=['GET', 'POST'])
 def form():
+    form = Text4ChangeForm()
     if request.method == 'POST':
-        input_text = request.form['text']
+        input_text = request.form['input_text']
+        print(input_text)
         return tp.handle_text(input_text)
     else:
-        return render_template('form.html')
+        return render_template('form.html', form=form)
 
 
 if __name__ == "__main__":
--- typograph.py
+++ typograph.py
@@ -1,9 +1,8 @@
 import re
+NON_BREAKING_SPACE = '\u00A0'
 
-non_breaking_space = '\u00A0'
 
-
-patterns4change_and_replacements = [
+PATTERNS4CHANGE_AND_REPLACEMENTS = [
     # replace quotes ''/"" to «»
     (re.compile(r'[\'\"](.*?)[\'\"]', re.DOTALL), r'«\1»'),
     # text without double spaces
@@ -12,7 +11,7 @@ patterns4change_and_replacements = [
     (re.compile(r'^(\s*)|(\s*)$', re.MULTILINE), ''),
     # linking (no break space) between short words/numbers and other words
     (re.compile(r'((^|\s|\b)(\d+|[a-zA-Za-яА-Я]{1,2})) '),
-     r'\1{}'.format(non_breaking_space)),
+     r'\1{}'.format(NON_BREAKING_SPACE)),
     # replace hyphen to long dash
     (re.compile(r' - '), r' — '),
     # replace hyphen to short dash in telephone numbers
@@ -21,6 +20,6 @@ patterns4change_and_replacements = [
 
 
 def handle_text(text):
-    for pattern, replacement in patterns4change_and_replacements:
+    for pattern, replacement in PATTERNS4CHANGE_AND_REPLACEMENTS:
         text = pattern.sub(replacement, text)
     return text
